50a216a3b6b7478c3ba793b8673c2de61b365323
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-07-04 11:34:22 +0200

    comply with editor-config

--- database/db-demos-webtest/src_test/db/demos/webtest/PlayerDemoWebTestIT.java
+++ database/db-demos-webtest/src_test/db/demos/webtest/PlayerDemoWebTestIT.java
@@ -1,8 +1,11 @@
 package db.demos.webtest;
 
-import static com.codeborne.selenide.Condition.*;
-
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.match;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.refresh;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -17,36 +20,36 @@ import com.axonivy.ivy.webtest.primeui.widget.SelectOneMenu;
 @IvyWebTest(headless = true)
 public class PlayerDemoWebTestIT {
 
-	@Test
-	public void fillDialogForm() {
-		open(EngineUrl.createProcessUrl("db-demos/184BD5B00AD6A83D/crudPlayerDialog.ivp"));
+  @Test
+  public void fillDialogForm() {
+    open(EngineUrl.createProcessUrl("db-demos/184BD5B00AD6A83D/crudPlayerDialog.ivp"));
 
-		$(By.id("Teams:teamInputForm:foundationDay_input")).sendKeys("11.11.2010");
-		$(By.id("Teams:teamInputForm:Teamname")).sendKeys("FCL");
+    $(By.id("Teams:teamInputForm:foundationDay_input")).sendKeys("11.11.2010");
+    $(By.id("Teams:teamInputForm:Teamname")).sendKeys("FCL");
 
-		$(By.xpath("//*[@id=\"Teams:teamListForm:team_data\"]/tr[1]/td[1]")).shouldBe(visible,
-				match("is a primary key", el -> isNumber(el)));
-		$(By.id("Teams:teamInputForm:saveTeam")).shouldBe(enabled).click();
-		$(By.id("Teams:teamListForm:team:1:delete")).should(enabled).click();
-		refresh();
-		$(By.id("Players:playerInputForm:name")).sendKeys("Tom");
-		$(By.id("Players:playerInputForm:Birthday_input")).sendKeys("11.11.2010");
-		$(By.id("Players:playerInputForm:Birthday_input")).sendKeys(Keys.TAB);
-		$(By.id("Players:playerInputForm:FavoriteColor")).sendKeys("Yellow");
-		SelectOneMenu menu = PrimeUi.selectOne(By.id("Players:playerInputForm:team"));
-		menu.selectItemByLabel("FCB");
-		
-		$(By.xpath("//*[@id=\"Players:playerListForm:player_data\"]/tr[1]/td[4]")).shouldBe(visible,
-				match("is a primary key", el -> isNumber(el)));
-		$(By.id("Players:playerListForm:player:0:delete")).should(enabled).click();
-		$(By.id("Players:playerInputForm:savePlayer")).shouldBe(enabled).click();
+    $(By.xpath("//*[@id=\"Teams:teamListForm:team_data\"]/tr[1]/td[1]")).shouldBe(visible,
+        match("is a primary key", this::isNumber));
+    $(By.id("Teams:teamInputForm:saveTeam")).shouldBe(enabled).click();
+    $(By.id("Teams:teamListForm:team:1:delete")).should(enabled).click();
+    refresh();
+    $(By.id("Players:playerInputForm:name")).sendKeys("Tom");
+    $(By.id("Players:playerInputForm:Birthday_input")).sendKeys("11.11.2010");
+    $(By.id("Players:playerInputForm:Birthday_input")).sendKeys(Keys.TAB);
+    $(By.id("Players:playerInputForm:FavoriteColor")).sendKeys("Yellow");
+    SelectOneMenu menu = PrimeUi.selectOne(By.id("Players:playerInputForm:team"));
+    menu.selectItemByLabel("FCB");
 
-	}
+    $(By.xpath("//*[@id=\"Players:playerListForm:player_data\"]/tr[1]/td[4]")).shouldBe(visible,
+        match("is a primary key", this::isNumber));
+    $(By.id("Players:playerListForm:player:0:delete")).should(enabled).click();
+    $(By.id("Players:playerInputForm:savePlayer")).shouldBe(enabled).click();
 
-	private boolean isNumber(WebElement element) {
-		String text = element.getText();
-		int z = Integer.parseInt(text);
-		return z >= 0;
-	}
+  }
+
+  private boolean isNumber(WebElement element) {
+    String text = element.getText();
+    int z = Integer.parseInt(text);
+    return z >= 0;
+  }
 
 }
