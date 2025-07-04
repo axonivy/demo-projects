b56d7295c68398d4e6666cbc9cdcbaa3fa5239b4
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-07-04 14:29:36 +0200

    add first simple jakarta.data repository impl

---
+++ database/db-demos/src/db/demos/persistence/PlayerRepo.java
@@ -0,0 +1,16 @@
+package db.demos.persistence;
+
+import java.util.List;
+
+import jakarta.data.repository.By;
+import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.Find;
+import jakarta.data.repository.Repository;
+
+@Repository(dataStore = "league")
+public interface PlayerRepo extends CrudRepository<Player, Integer> {
+
+  @Find
+  List<Player> findByLastName(@By("lastName") String lastName);
+
+}
