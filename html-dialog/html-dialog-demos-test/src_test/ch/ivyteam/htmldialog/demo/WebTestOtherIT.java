ae70dbfde28fe47fb3490e25cf5e97ead5d2f506
Lukas Lieb <lukas.lieb@axonivy.com>
2025-10-07 09:46:44 +0200

    Remove bootstrap demo

--- html-dialog/html-dialog-demos-test/src_test/ch/ivyteam/htmldialog/demo/WebTestOtherIT.java
+++ html-dialog/html-dialog-demos-test/src_test/ch/ivyteam/htmldialog/demo/WebTestOtherIT.java
@@ -10,11 +10,9 @@ import static com.codeborne.selenide.Selenide.$;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 
 @IvyWebTest
 public class WebTestOtherIT {
@@ -27,22 +25,6 @@ public class WebTestOtherIT {
     $(By.id("paintHtml:name")).shouldBe(visible).sendKeys("paintHtml");
     $(By.id("paintHtml:buttonShow")).shouldBe(visible, enabled).click();
     $(By.id("paintHtml:growl_container")).shouldHave(text("Hello paintHtml"));
-  }
-
-  @Test
-  public void testHtmlBootstrapDemo() {
-    startProcess("145D1862CF17F2C9/Html5BootstrapDemo.ivp");
-    $(By.id("Form:Name")).shouldBe(visible).sendKeys("name");
-    $(By.id("Form:Email")).sendKeys("email@ivyteam.ch");
-    ((JavascriptExecutor) WebDriverRunner.getWebDriver())
-            .executeScript("document.getElementById('Form:Birthdate').setAttribute('value', '2016-01-01')");
-    $(By.id("Form:Captcha")).sendKeys("21");
-    $(By.id("Submit")).click();
-    $(By.id("captchaError")).shouldHave(text("Error!"));
-    $(By.id("Form:Captcha")).shouldBe(visible).clear();
-    $(By.id("Form:Captcha")).sendKeys("42");
-    $(By.id("Submit")).click();
-    $(By.id("messageSent")).shouldHave(text("Hey name! Your message has been sent."));
   }
 
   @Test
