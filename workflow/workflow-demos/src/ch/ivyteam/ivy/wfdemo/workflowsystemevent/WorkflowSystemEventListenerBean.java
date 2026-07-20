29694d8d66e33c3d41742cc89bff854d307e79b5
Lukas Lieb <lukas.lieb@axonivy.com>
2026-07-20 08:39:46 +0200

    Fix deprecated api

--- workflow/workflow-demos/src/ch/ivyteam/ivy/wfdemo/workflowsystemevent/WorkflowSystemEventListenerBean.java
+++ workflow/workflow-demos/src/ch/ivyteam/ivy/wfdemo/workflowsystemevent/WorkflowSystemEventListenerBean.java
@@ -35,13 +35,13 @@ public class WorkflowSystemEventListenerBean extends AbstractProcessStartEventBe
   @Override
   public void start() throws ServiceException {
     super.start();
-    getEventBeanRuntime().getProcessModelVersion().getApplication().systemEventDispatcher()
+    getEventBeanRuntime().getProcessModelVersion().app().systemEventDispatcher()
         .addSystemEventListener(EnumSet.of(SystemEventCategory.WORKFLOW), this.listener);
   }
 
   @Override
   public void stop() throws ServiceException {
-    getEventBeanRuntime().getProcessModelVersion().getApplication().systemEventDispatcher()
+    getEventBeanRuntime().getProcessModelVersion().app().systemEventDispatcher()
         .removeSystemEventListener(EnumSet.of(SystemEventCategory.WORKFLOW), this.listener);
     super.stop();
   }
